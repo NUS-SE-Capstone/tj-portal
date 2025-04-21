8ca50355c780fd5a00ad4b85a01f15432c6f5ac0
whqwq <1776678487@qq.com>
2025-04-22 00:44:55 +0800

    main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+// 组件库
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import App from './App.vue'
-import router from './router'
+import '@/style/font_style/iconfont.css';
+// 全局样式
+import '@/style/index.scss';
+// 权限-路由拦截
+import './permission';
+// 数据存储、路由
+import { store } from './store';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(ElementPlus);
+app.use(store);
+app.use(router);
 
 app.mount('#app')
